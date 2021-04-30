1a958319df8abbc03d3d61159139275ab8802c36
LIL FOFO7 <78570650+LILFOFO@users.noreply.github.com>
2021-04-30 13:18:41 -0300

    Add files via upload

---
+++ settings/config.js
@@ -0,0 +1,6 @@
+const config = {
+        botName: 'O GRANDE FOFO',
+        ownerName: 'LIL FOFO/CARLIN',
+        youtube: 'https://www.youtube.com/c/LilFoFo/videos',
+        instagram: 'https://www.instagram.com/oglilfofo/',
+}
